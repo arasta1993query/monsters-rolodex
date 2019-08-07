f2a251c7e26a36125fd59ab9d58280175e04946f
arasta1993query <alias1371@gmail.com>
2019-08-08 02:32:01 +0430

    adding files for github pages

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React , {Component} from  'react';
+
+import {CardList} from './components/card-list/card-list.component';
+
+import {SearchBox} from './components/search-box/search-box.component';
+
+
+
+
+import './App.css';
+
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            monsters: [],
+            searchField: ''
+        };
+
+        
+    }
+
+componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({monsters:users}));
+}
+
+handleChange = e =>{
+    this.setState({searchField : e.target.value});
+}
+
+    render(){
+        
+        const {monsters, searchField} = this.state;
+        const filterMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+        return ( 
+                <div className = "App" >
+                    <h1>Monster Rolodex</h1>
+                    <SearchBox 
+                        placeholder = 'Search monsters' 
+                        handleChange={this.handleChange}
+                    />
+                    
+                    
+                    <CardList monsters={filterMonsters}/>
+                   
+                </div>
+            );
+        }
+        
+    
+}
+export default App;
